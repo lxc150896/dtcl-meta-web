0b9d3a2001e88c57e7434f22b103bededb235448
luong.xuan.cuong <luong.xuan.cuong@sun-asterisk.com>
2025-08-22 08:51:38 +0700

    fix error img rank

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['cdn.jsdelivr.net', 'cdn.lolchess.gg', 'ddragon.leagueoflegends.com', 'raw.communitydragon.org'],
+    domains: ['cdn.jsdelivr.net', 'cdn.lolchess.gg', 'ddragon.leagueoflegends.com', 'raw.communitydragon.org', 'ddragon.dakgg.net'],
     remotePatterns: [
       {
         protocol: 'https',
