99240330066ef0d682e92dd27c2c8f7a58156662
luong.xuan.cuong <luong.xuan.cuong@sun-asterisk.com>
2025-09-10 09:02:32 +0700

    add app-ads txt admod

---
+++ public/images/Map_files/main.jsx
@@ -0,0 +1,11 @@
+import __vite__cjsImport0_react_jsxDevRuntime from "/node_modules/.vite/deps/react_jsx-dev-runtime.js?v=291b99d8"; const jsxDEV = __vite__cjsImport0_react_jsxDevRuntime["jsxDEV"];
+import __vite__cjsImport1_react from "/node_modules/.vite/deps/react.js?v=291b99d8"; const React = __vite__cjsImport1_react.__esModule ? __vite__cjsImport1_react.default : __vite__cjsImport1_react;
+import __vite__cjsImport2_reactDom_client from "/node_modules/.vite/deps/react-dom_client.js?v=291b99d8"; const createRoot = __vite__cjsImport2_reactDom_client["createRoot"];
+import App from "/src/App.jsx?t=1756872777726";
+createRoot(document.getElementById("root")).render(/* @__PURE__ */ jsxDEV(App, {}, void 0, false, {
+  fileName: "C:/Users/luong.xuan.cuong/Documents/ibc-mapping-tool/src/main.jsx",
+  lineNumber: 5,
+  columnNumber: 52
+}, this));
+
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJtYXBwaW5ncyI6IkFBSW1EO0FBSm5ELE9BQU9BLFdBQVc7QUFDbEIsU0FBU0Msa0JBQWtCO0FBQzNCLE9BQU9DLFNBQVM7QUFFaEJELFdBQVdFLFNBQVNDLGVBQWUsTUFBTSxDQUFDLEVBQUVDLE9BQU8sdUJBQUMsU0FBRDtBQUFBO0FBQUE7QUFBQTtBQUFBLE9BQUksQ0FBRyIsIm5hbWVzIjpbIlJlYWN0IiwiY3JlYXRlUm9vdCIsIkFwcCIsImRvY3VtZW50IiwiZ2V0RWxlbWVudEJ5SWQiLCJyZW5kZXIiXSwiaWdub3JlTGlzdCI6W10sInNvdXJjZXMiOlsibWFpbi5qc3giXSwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IFJlYWN0IGZyb20gJ3JlYWN0JztcclxuaW1wb3J0IHsgY3JlYXRlUm9vdCB9IGZyb20gJ3JlYWN0LWRvbS9jbGllbnQnO1xyXG5pbXBvcnQgQXBwIGZyb20gJy4vQXBwLmpzeCc7XHJcblxyXG5jcmVhdGVSb290KGRvY3VtZW50LmdldEVsZW1lbnRCeUlkKCdyb290JykpLnJlbmRlcig8QXBwIC8+KTtcclxuIl0sImZpbGUiOiJDOi9Vc2Vycy9sdW9uZy54dWFuLmN1b25nL0RvY3VtZW50cy9pYmMtbWFwcGluZy10b29sL3NyYy9tYWluLmpzeCJ9
